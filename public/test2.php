43f52482bd8f72dff9cc014c415479b55dccc182
Vanni <web.cn@msn.com>
2019-09-23 06:50:54 +0800

    Update test2.php

--- public/test2.php
+++ public/test2.php
@@ -1,21 +1,53 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="/dist/bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/dist/bower_components/jquery-ui/themes/base/jquery-ui.css">
-    <script src="/dist/bower_components/jquery/dist/jquery.min.js"></script>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        .box{
+            width: 500px;
+            height: 200px;
+            background-color: #ccc;
+            border:1px solid black;
+            line-height: 200px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin-top: -100px;
+            margin-left: -200px;
+            text-align: center;
+            /*display:none;*/
+        }
+    </style>
+    <script>
+        window.onload = function(){
+            var oBox=document.querySelector('.box');
+            console.log(oBox);
+
+            document.addEventListener('dragover',function(ev){
+                oBox.style.display='block';
+                ev.preventDefault()
+            });
+            oBox.addEventListener('dragenter',function(){
+                oBox.innerHTML='请松手';
+            });
+            oBox.addEventListener('dragleave',function(){
+                console.log('bbb')
+                oBox.innerHTML='请拖到这里';
+            });
+
+            oBox.addEventListener('drop',function(ev){
+                console.log(ev.dataTransfer.files)
+                oBox.style.display='block';
+                console.log(ev);
+                ev.preventDefault();
+            });
+        };
+    </script>
 </head>
-
-
-
 <body>
-<div id="slider">sdfsdf</div>
-<script src="/dist/bower_components/jquery-ui/jquery-ui.min.js"></script>
-<script>
-// $(function(){
-    // $( "#slider" ).slider();
-    $( "input" ).slider()
-// });
-</script>
+<div class='box'>
+    请把文件拖到这里
+</div>
 </body>
 </html>
