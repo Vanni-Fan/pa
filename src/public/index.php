e201b9dcf03bbd14a66ba06ea771293eda4c8e59
Vanni Fan <web.cn@msn.com>
2020-04-12 22:16:04 +0800

    update

---
+++ src/public/index.php
@@ -0,0 +1,56 @@
+<?php
+//class Txt{
+//    static function log(...$data){
+//        $event = array_shift($data);
+//        switch($type = $event->getType()){
+//            case 'beforeCheckRoute':
+//            case 'notMatchedRoute':
+//            case 'matchedRoute':
+//                error_log("\n事件类型：".$type."; Route：[".$data[1]->getPattern().']',3,'/tmp/log.txt');
+//                break;
+//            case 'beforeCheckRoutes':
+//            default:
+//                $sources = [];
+//                foreach($data as $source) $sources[] = is_object($source) ? get_class($source) : print_r($source,1);
+//                error_log("\n\n事件类型：".$type."; 源于：[".implode(', ',$sources).']',3,'/tmp/log.txt');
+//        }
+//    }
+//}
+
+$app = include '/var/www/pa/public/index.php'; // 引入 PA 的 index.php 文件
+
+$app->init(
+    [
+        'trace' => 1, // 打开调试
+        'application' => __DIR__.'/..',
+#        'module_path' => __DIR__ .'/../modules',
+#        'domain_bind' => [
+#            'h5'=>'api,web',
+#            'www'=>'web',
+#            'api'=>'api',
+#            'in'=>'admin,api,web',
+#            'oa'=>'admin',
+#        ],
+	'pa_db'       => [
+		'adapter' => 'sqlite',
+		'dbname'  => POWER_DATA . '/powerdb.sql3.db',
+	        'prefix'  => ''
+	],
+#        'pa_url_path' => 'dashboard',
+//        'event'=>[
+//            'handler' => 'Txt::log',
+//            'events'=>[
+//                'router:matchedRoute',
+//                'router:beforeCheckRoutes',
+//                'router:beforeCheckRoute',
+//                'router:notMatchedRoute',
+//                'dispatch:beforeException',
+//                'application:boot',
+//                'loader:beforeCheckClass',
+//                'router:matchedRoute',
+//            ],
+//        ]
+    ]
+)->run();
+
+#print_r(PA::$router);
